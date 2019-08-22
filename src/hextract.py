a0c304fb747a4f861a6cebac399141da88a00ef6
Edwin Tandiono <tandiono98@gmail.com>
2019-08-23 00:19:00 +0700

    Allow multiple XSD imports

--- src/hextract.py
+++ src/hextract.py
@@ -16,59 +16,53 @@ import save
 # Generate hRESTS dictionary using config.ini
 def generateDictionary():
 	hrests_dict = {}
-
+	
+	importing_xsd = False
+	hrests_dict["importedXsd"] = {}
+	hrests_dict["localMessage"] = False
+	
 	try:
 		f = open("../config/config.ini", "r")
 		for line in f:
 			if len(line) > 1 and line[0] != '[':
 				k, v = line.split('=', 1)
-				hrests_dict[k] = v.rstrip().lower()
+				if importing_xsd:
+					hrests_dict["importedXsd"][k] = v.rstrip().lower().replace(" ", "").split(',')
+				else:
+					hrests_dict[k] = v.rstrip().lower()
+			elif "IMPORTED XSD" in line:
+				importing_xsd = True
+			elif len(line) > 1:
+				importing_xsd = False
 		f.close()
 	except IOError:
 		print("No configuration file found. Generating default configuration file at ../config/config.ini.")
 		print("Please set your xpath queries and other attributes before running this program again.")
 		f = open("../config/config.ini", 'w')
 		f.write("[XPATH QUERIES]\n")
-		f.write("service=service\n")
-		f.write("operation=operation\n")
-		f.write("method=method\n")
-		f.write("endpoint=endpoint\n")
-		f.write("input=input\n")
-		f.write("output=output\n")
-		f.write("param=param\n")
+		f.write("service=\n")
+		f.write("operation=\n")
+		f.write("method=\n")
+		f.write("endpoint=\n")
+		f.write("input=\n")
+		f.write("output=\n")
+		f.write("param=\n")
 		f.write("\n")
 		f.write("[WSDL 2.0 ATTRIBUTES]\n")
-		f.write("serviceName=id\n")
-		f.write("targetNamespace=http://example.com\n")
-		f.write("xsdnamespace=http://example.com\n")
-		f.write("schemaLocation=http://example.com\n")
+		f.write("serviceName=\n")
+		f.write("targetNamespace=\n")
+		f.write("\n")
+		f.write("[IMPORTED XSD]\n")
 		f.write("\n")
 		f.write("[CUSTOM ATTRIBUTES]\n")
-		f.write("operationName=id\n")
-		f.write("binding=data-binding\n")
-		f.write("type=data-type\n")
-		f.write("minOccurs=data-minoccurs\n")
-		f.write("maxOccurs=data-maxoccurs\n")
-		f.write("message=data-message\n")
+		f.write("operationName=\n")
+		f.write("binding=\n")
+		f.write("type=\n")
+		f.write("minOccurs=\n")
+		f.write("maxOccurs=\n")
+		f.write("message=\n")
 		f.write("\n")
 		f.close()
-
-		hrests_dict["service"] = "service"
-		hrests_dict["operation"] = "operation"
-		hrests_dict["method"] = "method"
-		hrests_dict["endpoint"] = "endpoint"
-		hrests_dict["input"] = "input"
-		hrests_dict["output"] = "output"
-		hrests_dict["param"] = "param"
-		hrests_dict["serviceName"] = "Example"
-		hrests_dict["operationName"] = "id"
-		hrests_dict["binding"] = "data-binding"
-		hrests_dict["type"] = "data-type"
-		hrests_dict["minOccurs"] = "data-minoccurs"
-		hrests_dict["maxOccurs"] = "data-maxoccurs"
-		hrests_dict["targetNamespace"] = "http://example.com"
-		hrests_dict["xsdnamespace"] = "http://example.com"
-		hrests_dict["schemaLocation"] = "http://example.com"
 
 		sys.exit(1)
 
@@ -81,39 +75,42 @@ def generateWSDL2(resources):
 	"""
 	xml += "targetNamespace=\"" + hrests_dict["targetNamespace"] + "\"\n"
 	xml += "	xmlns:tns=\"" + hrests_dict['targetNamespace'] + "\"\n"
-	if 'xsdnamespace' in hrests_dict:
-		xml += "	xmlns:msg=\"" + hrests_dict['xsdnamespace'] + "\"\n"
-	else:
-		xml += "	xmlns:msg=\"" + hrests_dict['targetNamespace'] + "\"\n"
+	if hrests_dict["importedXsd"]:
+		for xsd in hrests_dict["importedXsd"]:
+			xml += "	xmlns:" + xsd + "=\"" + hrests_dict["importedXsd"][xsd][0] + "\"\n"
 	xml += """	xmlns:whttp="http://www.w3.org/ns/wsdl/http"
 	xmlns:wsdlx="http://www.w3.org/ns/wsdl-extensions">
 
 	"""
-
 	xml += """<wsdl:types>
-    """
-	if "xsdnamespace" in resources:
-		xml += "<xsd:import xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"\n"
-		xml += "			namespace=\"" + hrests_dict["xsdnamespace"] + "\"\n"
-		xml += "			schemaLocation=\"" + hrests_dict["schemaLocation"] + "\"/>\n"
-	else:		
-		xml += "<xsd:schema xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"\n"
+"""
+	if hrests_dict["importedXsd"]:
+		for xsd in hrests_dict["importedXsd"]:
+			xml += "		<xsd:import xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"\n"
+			xml += "			namespace=\"" + hrests_dict["importedXsd"][xsd][0] + "\"\n"
+			xml += "			schemaLocation=\"" + hrests_dict["importedXsd"][xsd][1] + "\"/>\n"
+
+	if hrests_dict["localMessage"]:
+		xml += "		<xsd:schema xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"\n"
 		xml += "			targetNamespace=\"" + hrests_dict["targetNamespace"] + "\">\n"
 		for op in resources["operations"]:
-			xml += "			<xsd:element name=\"" + op["input"]["message"] + "\">\n"
-			xml += "				<xsd:complexType>\n"
-			xml += "					<xsd:sequence>\n"
-			for inp in op["input"]["params"]:
-				xml += "						<xsd:element name=\"" + inp["name"] + "\" type=\"xsd:" + inp["type"] + "\""
-				if "minOccurs" in inp:
-					xml += " minOccurs=\"" + inp["minOccurs"] + "\""
-				if "maxOccurs" in inp:
-					xml += " maxOccurs=\"" + inp["maxOccurs"] + "\""
-				xml += "/>\n"
-			xml += """					</xsd:sequence>		
+			if ":" not in op["input"]["message"]:
+				xml += "			<xsd:element name=\"" + op["input"]["message"] + "\">\n"
+				xml += "				<xsd:complexType>\n"
+				xml += "					<xsd:sequence>\n"
+				for inp in op["input"]["params"]:
+					xml += "						<xsd:element name=\"" + inp["name"] + "\" type=\"xsd:" + inp["type"] + "\""
+					if "minOccurs" in inp:
+						xml += " minOccurs=\"" + inp["minOccurs"] + "\""
+					if "maxOccurs" in inp:
+						xml += " maxOccurs=\"" + inp["maxOccurs"] + "\""
+					xml += "/>\n"
+				xml += """					</xsd:sequence>		
 				</xsd:complexType>
 	  	</xsd:element>
 """
+	
+		if ":" not in op["output"]["message"]:
 			xml += "			<xsd:element name=\"" + op["output"]["message"] + "\">\n"
 			xml += "				<xsd:complexType>\n"
 			xml += "					<xsd:sequence>\n"
@@ -128,7 +125,7 @@ def generateWSDL2(resources):
 				</xsd:complexType>
 	  	</xsd:element>
 	  """
-		xml += """</xsd:schema>
+		xml += """		</xsd:schema>
 """
 	xml += """	</wsdl:types>
 
@@ -144,8 +141,15 @@ def generateWSDL2(resources):
 			"""
 		xml += """style="http://www.w3.org/ns/wsdl/style/iri">
 			"""
-		xml += "<wsdl:input element=\"msg:" + op["input"]["message"] + "\"/>\n"
-		xml += "			<wsdl:output element=\"msg:" + op["output"]["message"] + "\"/>\n"
+		if ":" not in op["input"]["message"]:
+			xml += "<wsdl:input element=\"tns:" + op["input"]["message"] + "\"/>\n"
+		else:
+			xml += "<wsdl:input element=\"" + op["input"]["message"] + "\"/>\n"
+
+		if ":" not in op["output"]["message"]:
+			xml += "			<wsdl:output element=\"tns:" + op["output"]["message"] + "\"/>\n"
+		else:
+			xml += "			<wsdl:output element=\"" + op["output"]["message"] + "\"/>\n"
 		xml += "		</wsdl:operation>\n"
 	xml += """	</wsdl:interface>
 
@@ -284,16 +288,19 @@ def html2resourcesxpath(html_text, hrests_dict):
 			raise Exception("Error while extracting resources: targetNamespace can\'t be empty.")
 		if urlparse(hrests_dict["targetNamespace"]).scheme == "":
 			raise Exception("Error while extracting resources: target namespace must be a valid URI.")
-		if "xsdNamespace" in hrests_dict:
-			print("Warning: XSD Namespace found. Only 1 XSD can be imported.")
-			# resources["xsdnamespace"] = service.get(hrests_dict["xsdnamespace"]).replace(" ", "")
-			if urlparse(hrests_dict["xsdNamespace"]).scheme == "":
-				raise Exception("Error while extracting resources: xsd namespace must be a valid URI.")
-			# resources["schemaLocation"] = service.get(hrests_dict["schemaLocation"]).replace(" ","")
-			if len(hrests_dict["schemaLocation"]) == 0:
-				raise Exception("Error while extracting resources: schemaLocation can\'t be empty.")
-			elif not hrests_dict["schemaLocation"].endswith(".xsd") and urlparse(hrests_dict["schemaLocation"]).scheme == "":
-				print("Warning: schemaLocation is not of xsd format or valid URI.")			
+		if hrests_dict["importedXsd"]:
+			for xsd in hrests_dict["importedXsd"]:
+				# print("Warning: XSD Namespace found. Only 1 XSD can be imported.")
+				# resources["xsdnamespace"] = service.get(hrests_dict["xsdnamespace"]).replace(" ", "")
+				if len(hrests_dict["importedXsd"][xsd]) < 2:
+					raise Exception("Error while extracting resources: Each imported XSD must have namespace and schemaLocation")
+				if urlparse(hrests_dict["importedXsd"][xsd][0]).scheme == "":
+					raise Exception("Error while extracting resources: xsdNamespace \"" + hrests_dict["importedXsd"][xsd][0] + "\" is not a valid URI.")
+				# resources["schemaLocation"] = service.get(hrests_dict["schemaLocation"]).replace(" ","")
+				if len(hrests_dict["importedXsd"][xsd][1]) == 0:
+					raise Exception("Error while extracting resources: schemaLocation can\'t be empty.")
+				elif not hrests_dict["importedXsd"][xsd][1].endswith(".xsd") and urlparse(hrests_dict["importedXsd"][xsd][1]).scheme == "":
+					print("Warning: schemaLocation \"" + hrests_dict["importedXsd"][xsd][1] + "\" is not of xsd format or valid URI.")			
 		resources["operations"] = []
 
 		# if len((hrests_dict["operation"])) > 0:
@@ -329,28 +336,35 @@ def html2resourcesxpath(html_text, hrests_dict):
 				else:
 					inpObj["message"] = op["name"] + "Request"
 					print("Warning: no message name specified for " + op["name"] + ", resolved using default name " + inpObj["message"])
-				if "xsdNamespace" not in hrests_dict:
-					for input in inputs.xpath("." + hrests_dict["param"]):
-						inp ={}
-						inp["name"] = input.text_content().replace(" ", "")
-						if len(inp["name"]) == 0:
-							print("Warning: input parameter is empty.")
-						if hrests_dict["type"] in input.attrib:
-							inp["type"] = input.get(hrests_dict["type"])
-						else:
-							inp["type"] = "string"
-							print("Warning: no type specified for " + inp["name"] + " of " + op["name"] + ", resolved using default type string")
-						if inp["type"] not in xsdTypes:
-							raise Exception("Error while parsing operation " + op["name"] + ": invalid datatype for param " + inp["name"] + ".")
-						if hrests_dict["minOccurs"] in input.attrib:
-							inp["minOccurs"] = input.get(hrests_dict["minOccurs"])
-							if not inp["minOccurs"].isdigit() and not inp["minOccurs"] == "unbounded":
-								raise Exception("Error while parsing operation " + op["name"] + ": minOccurs for param " + inp["name"] + " must be a positive integer.")
-						if hrests_dict["maxOccurs"] in input.attrib:
-							inp["maxOccurs"] = input.get(hrests_dict["maxOccurs"])
-							if not inp["maxOccurs"].isdigit() and not inp["maxOccurs"] == "unbounded":
-								raise Exception("Error while parsing operation " + op["name"] + ": maxOccurs for param " + inp["name"] + " must be a positive integer.")
-						inpObj["params"].append(inp)
+
+				if ":" not in inpObj["message"] and not hrests_dict["localMessage"]:
+					hrests_dict["localMessage"] = True
+				else:
+					xsd, message = inpObj["message"].split(':', 1)
+					if hrests_dict["importedXsd"] and not messageExistInXSD(message, hrests_dict["importedXsd"][xsd][1]):
+						print("Warning: Couldn't find \"" + message + "\" in \"" + hrests_dict["importedXsd"][xsd][1] + "\".")
+
+				for input in inputs.xpath("." + hrests_dict["param"]):
+					inp ={}
+					inp["name"] = input.text_content().replace(" ", "")
+					if len(inp["name"]) == 0:
+						print("Warning: input parameter is empty.")
+					if hrests_dict["type"] in input.attrib:
+						inp["type"] = input.get(hrests_dict["type"])
+					else:
+						inp["type"] = "string"
+						print("Warning: no type specified for " + inp["name"] + " of " + op["name"] + ", resolved using default type string")
+					if inp["type"] not in xsdTypes:
+						raise Exception("Error while parsing operation " + op["name"] + ": invalid datatype for param " + inp["name"] + ".")
+					if hrests_dict["minOccurs"] in input.attrib:
+						inp["minOccurs"] = input.get(hrests_dict["minOccurs"])
+						if not inp["minOccurs"].isdigit() and not inp["minOccurs"] == "unbounded":
+							raise Exception("Error while parsing operation " + op["name"] + ": minOccurs for param " + inp["name"] + " must be a positive integer.")
+					if hrests_dict["maxOccurs"] in input.attrib:
+						inp["maxOccurs"] = input.get(hrests_dict["maxOccurs"])
+						if not inp["maxOccurs"].isdigit() and not inp["maxOccurs"] == "unbounded":
+							raise Exception("Error while parsing operation " + op["name"] + ": maxOccurs for param " + inp["name"] + " must be a positive integer.")
+					inpObj["params"].append(inp)
 			else:
 				inpObj["message"] = op["name"] + "Request"
 				print("Warning: no message name specified for " + op["name"] + ", resolved using default name " + inpObj["message"])
@@ -373,29 +387,35 @@ def html2resourcesxpath(html_text, hrests_dict):
 						print("Warning: no message name specified for " + op["name"] + ", resolved using default name " + outObj["message"])
 				else:
 					outObj["message"] = op["name"] + "Response"
-					print("Warning: no message name specified for " + op["name"] + ", resolved using default name " + outObj["message"])
-				if "xsdNamespace" not in hrests_dict:	
-					for output in outputs.xpath("." + hrests_dict["param"]):
-						out ={}
-						out["name"] = output.text_content().replace(" ", "")
-						if len(out["name"]) == 0:
-							print("Warning: output parameter name is empty.")
-						if hrests_dict["type"] in output.attrib:
-							out["type"] = output.get(hrests_dict["type"])
-						else:
-							out["type"] = "string"
-							print("Warning: no type specified for " + out["name"] + " of " + op["name"] + ", resolved using default type string")
-						if out["type"] not in xsdTypes:
-							raise Exception("Error while parsing operation " + op["name"] + ": invalid datatype for param " + out["name"] + ".")
-						if hrests_dict["minOccurs"] in output.attrib:
-							out["minOccurs"] = output.get(hrests_dict["minOccurs"])
-							if not out["minOccurs"].isdigit() and not out["minOccurs"] == "unbounded":
-								raise Exception("Error while parsing operation " + op["name"] + ": minOccurs for param " + out["name"] + " must be a positive integer.")
-						if hrests_dict["maxOccurs"] in output.attrib:
-							out["maxOccurs"] = output.get(hrests_dict["maxOccurs"])
-							if not out["maxOccurs"].isdigit() and not out["maxOccurs"] == "unbounded":
-								raise Exception("Error while parsing operation " + op["name"] + ": maxOccurs for param " + out["name"] + " must be a positive integer.")
-						outObj["params"].append(out)
+					print("Warning: no message name specified for " + op["name"] + ", resolved using default name " + outObj["message"])	
+				if ":" not in outObj["message"] and not hrests_dict["localMessage"]:
+					hrests_dict["localMessage"] = True
+				else:
+					xsd, message = outObj["message"].split(':', 1)
+					if hrests_dict["importedXsd"] and not messageExistInXSD(message, hrests_dict["importedXsd"][xsd][1]):
+						print("Warning: Couldn't find \"" + message + "\" in \"" + hrests_dict["importedXsd"][xsd][1] + "\".")
+
+				for output in outputs.xpath("." + hrests_dict["param"]):
+					out ={}
+					out["name"] = output.text_content().replace(" ", "")
+					if len(out["name"]) == 0:
+						print("Warning: output parameter name is empty.")
+					if hrests_dict["type"] in output.attrib:
+						out["type"] = output.get(hrests_dict["type"])
+					else:
+						out["type"] = "string"
+						print("Warning: no type specified for " + out["name"] + " of " + op["name"] + ", resolved using default type string")
+					if out["type"] not in xsdTypes:
+						raise Exception("Error while parsing operation " + op["name"] + ": invalid datatype for param " + out["name"] + ".")
+					if hrests_dict["minOccurs"] in output.attrib:
+						out["minOccurs"] = output.get(hrests_dict["minOccurs"])
+						if not out["minOccurs"].isdigit() and not out["minOccurs"] == "unbounded":
+							raise Exception("Error while parsing operation " + op["name"] + ": minOccurs for param " + out["name"] + " must be a positive integer.")
+					if hrests_dict["maxOccurs"] in output.attrib:
+						out["maxOccurs"] = output.get(hrests_dict["maxOccurs"])
+						if not out["maxOccurs"].isdigit() and not out["maxOccurs"] == "unbounded":
+							raise Exception("Error while parsing operation " + op["name"] + ": maxOccurs for param " + out["name"] + " must be a positive integer.")
+					outObj["params"].append(out)
 			else:
 				outObj["message"] = op["name"] + "Response"
 				print("Warning: no message name specified for " + op["name"] + ", resolved using default name " + outObj["message"])
@@ -415,6 +435,33 @@ def html2resourcesxpath(html_text, hrests_dict):
 		print(exc_type, fname, exc_tb.tb_lineno)
 		print(resources)
 		sys.exit()
+
+# Check if message exist in XSD
+def messageExistInXSD(messageName, schemaLocation):
+	xsd_text = ""
+	if urlparse(schemaLocation).scheme != "":
+		try:
+			xsd_text = str.encode(requests.get(schemaLocation).text)
+		except:
+			print("Error while reading schema \"" + schemaLocation + "\".")
+			return False
+		if isinstance (xsd_text, tuple):
+			print("Error while reading schema \"" + schemaLocation + "\": " + xsd_text[0])
+			print('%s : %s' % (html_text[1], html_text[2]))
+			return False
+	else:
+		try:
+			with open('schemaLocation', 'r') as file:
+				xsd_text = file.read()
+		except:
+			print("Error while reading schema \"" + schemaLocation + "\".")
+			return False
+	root = etree.fromstring(xsd_text)
+	elm = root.xpath("//*[@name='" + messageName + "']")
+	if len(elm) > 0:
+		return True
+	else:
+		return False
 
 # Main Program
 if len(sys.argv) < 2:
